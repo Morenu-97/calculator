55ca1719aa5bc737ccf0bb5e474b6dc71ac11386
Morenu-97 <brayan1897mendez@gmail.com>
2022-07-03 13:46:34 -0500

    add new functions: -Radication. -Absolute value.

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
@@ -6,34 +9,37 @@ public class Main {
         while (true) {
             options();
             int n = kb.nextInt();
-
             if (powerOff(n)) break;
-
-            System.out.println("Enter two numbers");
-            double a = kb.nextDouble();
-            double b = kb.nextDouble();
-
-            operations(n, a, b);
+            if (n < 5){
+                System.out.println("Enter two numbers");
+                double a = kb.nextDouble();
+                double b = kb.nextDouble();
+                operations(n,a,b);
+            } else if (n == 5 || n == 6) {
+                System.out.println("Enter one number");
+                double c = kb.nextDouble();
+                operations2(n,c);
+            }
         }
     }
     //    M. options
     public static void options () {
-        System.out.println("1=(+)\n2=(-)\n3=(*)\n4=(/)\n5=(off)");
+        System.out.println("1=(+)\n2=(-)\n3=(*)\n4=(/)\n5=(√)\n6=(∣a∣)\n7=(off)");
     }
 
     //     M. powerOff
     public static boolean powerOff(int option) {
-        if (option == 5) {
+        if (option == 7) {
             System.out.println("Power off");
             return true;
-        } else if (option > 5) {
+        } else if (option > 7) {
             System.out.println("Power off");
             return true;
         }
         return false;
     }
     //    M. Conditional
-    public static void operations(int n, double a, double b) {
+    public static void operations(int n, double a,double b) {
         if (n == 4) {
             if (b == 0) {
                 System.out.println("Can not be divided by zero");
@@ -48,9 +54,19 @@ public class Main {
             System.out.println(multiply(a,b));
         }
     }
+    //    M. Conditional
+    public static void operations2(int n, double c) {
+        if (n == 5) {
+            System.out.println(sqrt(c));
+        } else if (n == 6) {
+            System.out.println(absoluteValue(c));
+        }
+    }
     //    M. operations
     public static double divide(double a, double b) {return a / b;}
     public static double sum(double a, double b) {return a + b;}
     public static double subtract(double a, double b) {return a - b;}
     public static double multiply(double a, double b) {return a * b;}
+    public static double sqrt(double c) {return  Math.sqrt(c);}
+    public static double absoluteValue(double c) {return  Math.abs(c);}
 }
